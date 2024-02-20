4824a879aee406dcf9a03e4f33b931e9bf2039f8
Gean Marcos Geronymo <gean.geronymo@gmail.com>
2024-02-20 13:50:57 -0300

    atualizacao

--- sato.py
+++ sato.py
@@ -1,9 +1,10 @@
 # Programa para ler condicoes ambientais do termohigrometro
-# Rotronic Hygropalm 2 via interface RS-232
+# Termohigrometro SATO via RS-232 (USB)
 # Autor: Gean M. Geronymo
-# Data: 20/04/2016
-# Última atualizacao: 10/02/2021
-# removida interface direta com postgresql - salvar apenas via http na API REST
+# Data: 10/02/2021
+# salva log em TXT e envia para API REST
+
+# correcao de calibracao desabilitada provisoriamente
 
 # funcoes
 
@@ -19,18 +20,21 @@ def log_txt(ano,data,hora,humidity,temperature):
         text_file.close();
 
     return
-
-def write_buffer(filename,temperature,humidity,timestamp,certificado,data_certificado):
+	
+def write_buffer(filename,temperature,humidity,timestamp):
+#def write_buffer(filename,temperature,humidity,timestamp,certificado,data_certificado):
     # filename: string com o nome do arquivo de buffer
     with open(filename,"a") as csvfile:
         write_buffer = csv.writer(csvfile, delimiter=',',lineterminator='\n')
-        write_buffer.writerow([str(temperature),str(humidity),timestamp,certificado,data_certificado])
+        write_buffer.writerow([str(temperature),str(humidity),timestamp])
+		#write_buffer.writerow([str(temperature),str(humidity),timestamp,certificado,data_certificado])
         csvfile.close();
     return
 
 def open_buffer(filename):
     with open(filename) as csvfile:
-        reader = csv.DictReader(csvfile,delimiter=',',fieldnames=['temperature','humidity','date','certificado','data_certificado'])
+        reader = csv.DictReader(csvfile,delimiter=',',fieldnames=['temperature','humidity','date'])
+		#reader = csv.DictReader(csvfile,delimiter=',',fieldnames=['temperature','humidity','date','certificado','data_certificado'])
         d = list(reader)
         csvfile.close();
     return d
@@ -51,35 +55,29 @@ def dberror_log(timestamp):
         text_file.close();
     return
 
-def corr_temp(cal):
-    # calcula correcoes para temperatura
-    x_temperature = cal['Temperatura']['indicacoes'].split(',');
-    x_temperature = array([float(a) for a in x_temperature]);
-    temperature_correcoes = cal['Temperatura']['correcoes'].split(',');
-    temperature_correcoes = array([float(a) for a in temperature_correcoes]);
-    y_temperature = x_temperature + temperature_correcoes;
-    # minimos quadrados
-    A = vstack([x_temperature, ones(len(x_temperature))]).T;
-    a, b = linalg.lstsq(A, y_temperature)[0]
-    # temperature = a1*x + b1
-    return {'a':a, 'b':b}
+# def corr_temp(cal):
+    # x_temperature = cal['Temperatura']['indicacoes'].split(',');
+    # x_temperature = array([float(a) for a in x_temperature]);
+    # temperature_correcoes = cal['Temperatura']['correcoes'].split(',');
+    # temperature_correcoes = array([float(a) for a in temperature_correcoes]);
+    # y_temperature = x_temperature + temperature_correcoes;
+    # A = vstack([x_temperature, ones(len(x_temperature))]).T;
+    # a, b = linalg.lstsq(A, y_temperature)[0]
+    # return {'a':a, 'b':b}
 
-def corr_umid(cal):
-    # calcula correcoes para a umidade
-    x_humidity = cal['Umidade']['indicacoes'].split(',');
-    x_humidity = array([float(a) for a in x_humidity]);
-    humidity_correcoes = cal['Umidade']['correcoes'].split(',');
-    humidity_correcoes = array([float(a) for a in humidity_correcoes]);
-    y_humidity = x_humidity + humidity_correcoes;
-    # minimos quadrados
-    A = vstack([x_humidity, ones(len(x_humidity))]).T;
-    a, b = linalg.lstsq(A, y_humidity)[0]
-    # humidity = a2*x + b2
-    return {'a':a, 'b':b}
+# def corr_umid(cal):
+    # x_humidity = cal['Umidade']['indicacoes'].split(',');
+    # x_humidity = array([float(a) for a in x_humidity]);
+    # humidity_correcoes = cal['Umidade']['correcoes'].split(',');
+    # humidity_correcoes = array([float(a) for a in humidity_correcoes]);
+    # y_humidity = x_humidity + humidity_correcoes;
+    # A = vstack([x_humidity, ones(len(x_humidity))]).T;
+    # a, b = linalg.lstsq(A, y_humidity)[0]
+    # return {'a':a, 'b':b}
 
 def query_serial(serialconfig):
     # configuracao da conexao serial
-    # seguindo informacoes do manual do fabricante
+    # adaptado para termohigrometro SATO
     ser = serial.Serial(
         port=serialconfig['port'],
         baudrate=19200,
@@ -89,36 +87,29 @@ def query_serial(serialconfig):
         timeout = int(serialconfig['timeout'])
     )
 
-    querystring = serialconfig['querystring']
-    # envia para o termohigrometro a query string com o terminador de linha \r
-    #ser.write(b'{u00RDD}\r')
-    ser.write((querystring+'\r').encode())
-
-    # le a resposta do termohigrometro
-    rcv_str = ser.read(50)
+    # le uma linha do termohigrometro
+    rcv_str = ser.readline()
     # fecha a conexao serial
     ser.close()
 
     # transformar o byte object recebido em uma string
     dec_str = rcv_str.decode('utf-8')
     # processa a string para extrair os valores de temperatura e umidade
-    # o termohigrometro retorna uma string do tipo:
-    # '{u00RDD 0071.68;0022.78;----.--;----.--;#6\r'
-    # o comando abaixo remove o trecho
-    # '{u00RDD '
-    # Assim, e possivel separar temperatura e umidade utilizando ';'
-    data_array = (dec_str.replace(querystring.replace('}',' '),'')).split(';')
-    # arredonda a temperatura e umidade para uma casa decimal
-    #temperature = round(float(data_array[1]),1)
-    #humidity = round(float(data_array[0]),1)
+    data = dec_str.split()
+    temperature = float(data[1].replace(',',''))/10
+    humidity = float(data[2])/10
+	
+    data_array = [str(humidity), str(temperature)]
     return data_array
 
-def salvar_http(date, temperature, humidity, cal, url, api_key):
+def salvar_http(date, temperature, humidity, url, api_key):
+#def salvar_http(date, temperature, humidity, cal, url, api_key):
     # dados do certificado de calibracao do termohigrometro
-    certificado = cal['Certificado']['certificado']
-    data_certificado = cal['Certificado']['data']
+    #certificado = cal['Certificado']['certificado']
+    #data_certificado = cal['Certificado']['data']
     # escreve no buffer de saida
-    write_buffer("http_buffer.txt",temperature,humidity,date,certificado,data_certificado)
+    write_buffer("http_buffer.txt",temperature,humidity,date)
+	#write_buffer("http_buffer.txt",temperature,humidity,date,certificado,data_certificado)
     try:
         d = open_buffer("http_buffer.txt")
         for leitura in d:
@@ -126,8 +117,8 @@ def salvar_http(date, temperature, humidity, cal, url, api_key):
                 'temperature' : leitura['temperature'],
                 'humidity' : leitura['humidity'],
                 'date' : leitura['date'],
-                'certificado' : leitura['certificado'],
-                'data_certificado' : leitura['data_certificado']
+                #'certificado' : leitura['certificado'],
+                #'data_certificado' : leitura['data_certificado']
             }
             request = Request(url, urlencode(post_fields).encode())
             request.add_header('X-API-KEY', api_key)
@@ -143,7 +134,7 @@ def salvar_http(date, temperature, humidity, cal, url, api_key):
 if __name__ == "__main__":
 
     # carrega as bibliotecas necessarias
-    from numpy import array, linalg, arange, ones, vstack 
+    #from numpy import array, linalg, arange, ones, vstack 
     import serial       # interface RS-232
     import time
     import datetime     # funcoes de data e hora
@@ -162,21 +153,25 @@ if __name__ == "__main__":
     api_key = config['HttpConfig']['api_key']
 
     # o arquivo cal.ini reune as informacoes da calibracao do termohigrometro
-    cal = configparser.ConfigParser()
-    cal.read('cal.ini')
+    #cal = configparser.ConfigParser()
+    #cal.read('cal.ini')
 
     # calcular correcoes temperatura
-    coeff_temp = corr_temp(cal)
+    #coeff_temp = corr_temp(cal)
 
     # calcular correcoes umidade
-    coeff_umid = corr_umid(cal)
+    #coeff_umid = corr_umid(cal)
 
     data_array = query_serial(config['SerialConfig'])
 
     # valores corrigidos com o certificado de calibracao
-    temperature = round((coeff_temp['a']*float(data_array[1]) + coeff_temp['b']),1)
-    humidity = round((coeff_temp['a']*float(data_array[0]) + coeff_temp['b']),1)
-
+    #temperature = round((coeff_temp['a']*float(data_array[1]) + coeff_temp['b']),1)
+    #humidity = round((coeff_temp['a']*float(data_array[0]) + coeff_temp['b']),1)
+	
+	# valores sem correcao
+    temperature = round(float(data_array[1]),1)
+    humidity = round(float(data_array[0]),1)
+	
     # data e hora
     data_atual = data_hora();
     
@@ -184,5 +179,6 @@ if __name__ == "__main__":
     log_txt(data_atual['ano'],data_atual['data'],data_atual['hora'],humidity,temperature)
 
     # salvar no banco de dados (via API REST)
-    salvar_http(data_atual['timestamp'], temperature, humidity, cal, url, api_key)
+    salvar_http(data_atual['timestamp'], temperature, humidity, url, api_key)
+    #salvar_http(data_atual['timestamp'], temperature, humidity, cal, url, api_key)
 
